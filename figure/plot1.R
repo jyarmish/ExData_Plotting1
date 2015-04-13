f2ff3e6a5e13ce41444f159e356bf7582a54a0b1
jyarmish <jason-j1@juno.com>
2015-04-13 15:04:09 -0400

    Signed-off-by: jyarmish <jason-j1@juno.com>

---
+++ figure/plot1.R
@@ -0,0 +1,6 @@
+x<-read.table("household_power_consumption.txt",header=TRUE,sep=";")
+#head(x)
+x<-x[(x[,1]=="01/02/2007")|(x[,1]=="02/02/2007"),]
+png(filename = "plot1.png", width=480, height=480)
+hist(x$Global_active_power,col="red",xlab="Global Active Power (kilowatts)",ylab="Frequency",main="Global Active Power")
+dev.off()
